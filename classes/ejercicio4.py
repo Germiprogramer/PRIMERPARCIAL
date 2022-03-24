0d6f7ad386df3c333cb1fad569294046405bb174
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-24 08:42:43 +0100

    atributos ej 4

---
+++ classes/ejercicio4.py
@@ -0,0 +1,7 @@
+class Cuenta_bancaria:
+    def __init__(self, ID, nombre_titular, fecha_de_apertura, numero_cuenta, saldo):
+        self.ID = ID
+        self.nombre_titular = nombre_titular
+        self.fecha_de_apertura = fecha_de_apertura
+        self.numero_cuenta = numero_cuenta
+        self.saldo = saldo
