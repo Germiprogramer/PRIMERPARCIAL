d8b5f939e42dc819f5d35fe00882ffaabf6818d7
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-24 10:04:36 +0100

    cuentabancariaplazofijo funcion

--- EJERCICIO3/cuenta_bancaria_plazofijo.py
+++ EJERCICIO3/cuenta_bancaria_plazofijo.py
@@ -1,5 +1,22 @@
 from cuenta_bancaria import *
 
 class Cuenta_bancaria_plazofijo(Cuenta_bancaria):
-    def __init__(self, ID, nombre_titular, fecha_de_apertura, numero_cuenta, saldo):
+    def __init__(self, ID, nombre_titular, fecha_de_apertura, numero_cuenta, saldo, fecha_de_vencimiento):
         super().__init__(ID, nombre_titular, fecha_de_apertura, numero_cuenta, saldo)
+        self.fecha_de_vencimiento = fecha_de_vencimiento
+
+    
+    def retirar(self):
+        dinero = int(input("¿¿Cuanta pasta quieres sacar?? "))
+        fecha = input("Es antes de la fecha de vencimiento??  si / no")
+
+        if fecha == "si":
+            dinero = dinero + dinero * 0.05
+        else:
+            pass
+
+        if dinero>self.saldo:
+            pass
+        else:
+            self.saldo = self.saldo - dinero
+        return self.saldo
